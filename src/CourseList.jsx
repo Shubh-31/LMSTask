c8a04992a81290411dadd753275b136afeaf7cb2
Shubh-31 <shubhangmishra999@gmail.com>
2025-03-24 18:43:11 +0530

    updated ui

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -24,9 +24,12 @@ const CourseList = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 p-10">
-      <h1 className="text-3xl font-bold text-center text-blue-600 mb-8">
+      <h1 className="text-3xl font-bold text-center text-blue-700 mb-8">
         Available Courses
       </h1>
+      <h4 className="text-lg font-semibold text-center text-gray-600 mb-8">
+        Learning Made Easy
+      </h4>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {loading
           ? Array(6).fill(0).map((_, i) => <SkeletonLoader key={i} />) 
@@ -41,12 +44,12 @@ const CourseList = () => {
                   <p className="text-gray-600 mt-2">{course.description}</p>
                   
                 
-                  <p className="mt-2 text-lg font-medium text-blue-600">
+                  <p className="mt-2 text-sm font-semibold text-gray-600">
                     {completionPercentage > 0 ? `You completed ${completionPercentage}% of the course` : "Not started yet"}
                   </p>
 
                   <button
-                    className="mt-4 w-full bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition cursor-pointer"
+                    className="mt-4 w-full bg-blue-500 text-gray px-4 py-2 rounded-lg hover:bg-blue-600 hover:border-blue hover:rounded-lg transition cursor-pointer text-sm font-semibold hover:underline"
                     onClick={() => navigate(`/course/${course.id}`)}
                   >
                     View Chapters
